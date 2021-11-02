c39a0564dea8c30e8fab99279f372d7113faa0f2
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2021-11-02 15:35:15 +0000

    Broke it even more I guess

--- Assets/Scripts/tilePathFinder.cs
+++ Assets/Scripts/tilePathFinder.cs
@@ -73,32 +73,29 @@ public class tilePathFinder : MonoBehaviour {
 
 		Instantiate(pathSquare, new Vector3(target.x + 0.5f, target.y + 0.5f), Quaternion.identity);
 
-		if (FindPathSub(start - new Vector2Int(1, 1), target, processed, path, 0.0f, ref time, ref state)) {
+		int count = 0;
+		if (FindPathSub(start - new Vector2Int(1, 1), target, processed, new Hashtable(), path, 0.0f, ref time, ref state, ref count)) {
 			state = "foundPath";
 			return path;
 		}
 		return null;
     }
-    private bool FindPathSub(Vector2Int currentPosition2, Vector2Int target2, Hashtable processed, List<Vector2Int> path, float distanceTravelledAway, ref int time, ref string state) {
+    private bool FindPathSub(Vector2Int currentPosition2, Vector2Int target2, Hashtable oldProcessed, Hashtable newProcessed, List<Vector2Int> path, float distanceTravelledAway, ref int time, ref string state, ref int count) {
 		Vector3Int currentPosition3 = tilemap.WorldToCell(new Vector3(currentPosition2.x, currentPosition2.y));
 		Vector3Int target3 = tilemap.WorldToCell(new Vector3(target2.x, target2.y));
 
 		string key = currentPosition2.x + "," + currentPosition2.y;
-		if (processed[key] != null) {
+		if (oldProcessed[key] != null) {
 			state = "deadEnd";
 			return false;
-			
 		};
-        processed[key] = 1;
+		newProcessed[key] = 1;
+		count++;
 
 		GameObject square = Instantiate(pathSquare, new Vector3(currentPosition2.x + 0.5f, currentPosition2.y + 0.5f), Quaternion.identity);
-		float size = Mathf.Min((processed.Count / 150.0f) + 0.1f, 1);
+		float size = Mathf.Min((count / 150.0f) + 0.1f, 1);
 		square.transform.localScale = new Vector3(size, size, 1);
-		square.transform.Rotate(new Vector3(0, 0, -((processed.Count * 20) % 90)));
-
-		if (currentPosition2.x == -3 && currentPosition2.y == 3) {
-			Debug.Log("A");
-        }
+		square.transform.Rotate(new Vector3(0, 0, -((count * 20) % 90)));
 
 		if (Vector3Int.Distance(currentPosition3, target3) > maxSearchDistance) {
 			state = "reachedMaxSearch";
@@ -108,50 +105,36 @@ public class tilePathFinder : MonoBehaviour {
 			state = "reachedMaxTravelledAway";
 			return false;
 		}
-		if (processed.Count > maxTilesSearch) {
+		if (count > maxTilesSearch) {
 			state = "reachedMaxProcessed";
 			return false;
 		}
 
 		Vector2Int[] directions = new Vector2Int[4];
 		int[] directionTypes = new int[4];
+		Vector2Int direction2 = new Vector2Int();
 		path.Add(currentPosition2);
 		if (
 			currentPosition2.x == target2.x
 			&& currentPosition2.y == target2.y
 		) return true;
 
-		bool falling = isPassable(GetTileName(currentPosition3 + Vector3Int.down));
-		if (falling) {
-			currentPosition2 += Vector2Int.down;
-			currentPosition3 += Vector3Int.down;
-
-			if (
-				currentPosition2.x == target2.x
-				&& currentPosition2.y == target2.y
-			) {
-				path.Add(currentPosition2);
-				return true;
-			}
-		}
-
 		int index = 0;
 		int jumpIndex = -1;
-		Vector2Int direction2 = Vector2Int.left;
-		if (isPassable(GetTileName(currentPosition3 + (Vector3Int)direction2))) {
-			directions[index] = currentPosition2 + direction2;
+
+		if (TestDirection(currentPosition2, Vector2Int.left, ref direction2)) {
+			directions[index] = direction2;
 			directionTypes[index] = 0;
 			index++;
 		}
-		direction2 = Vector2Int.right;
-		if (isPassable(GetTileName(currentPosition3 + (Vector3Int)direction2))) {
-			directions[index] = currentPosition2 + direction2;
+		if (TestDirection(currentPosition2, Vector2Int.right, ref direction2)) {
+			directions[index] = direction2;
 			directionTypes[index] = 1;
 			index++;
 		}
 
-		if (falling) { // Can just fall
-			directions[index] = currentPosition2;
+		if (isPassable(GetTileName(currentPosition3 + Vector3Int.down))) { // Can just fall
+			directions[index] = currentPosition2 + Vector2Int.down;
 			directionTypes[index] = 3;
 			index++;
 		}
@@ -167,7 +150,7 @@ public class tilePathFinder : MonoBehaviour {
 
 				directions[index] = currentPosition2 + direction2;
 				jumpIndex = index;
-				directionTypes[index] = 3;
+				directionTypes[index] = 2;
 				index++;
 			}
 		}
@@ -179,6 +162,7 @@ public class tilePathFinder : MonoBehaviour {
 
 
 		List<Vector2Int>[] newPaths = new List<Vector2Int>[2];
+		Hashtable[] newProcessedDeluxe = new Hashtable[2];
 		int[] newTimes = new int[2];
 		int pathCount = 1;
 		bool[] outputs = new bool[2];
@@ -188,20 +172,26 @@ public class tilePathFinder : MonoBehaviour {
 		List<Vector2Int> newPath;
 		// Shortest valid route in the short term
 		for (int i = 0; i < index; i++) { // This is less efficient than using a proper sorting algorithm, however, most of the time it won't need to be fully sorted
-			min = 0;
+			min = Mathf.Infinity;
 			minIndex = -1;
 			for (int c = i; c < index; c++) {
 				float distance = Vector2Int.Distance(target2, directions[c]);
-				if (distance < min || c == i) {
+				if (distance < min) {
 					min = distance;
 					minIndex = c;
 				}
 			}
 
 			newPath = new List<Vector2Int>();
+			newProcessedDeluxe[0] = new Hashtable();
 			int directionType = directionTypes[minIndex];
 			newTimes[0] = actionTimes[directionType];
-			outputs[0] = FindPathSub(directions[minIndex], target2, processed, newPath, GetDistanceTravelledAway(distanceTravelledAway, currentPosition2, target2, directions[minIndex]), ref newTimes[0], ref state);
+
+			Hashtable alreadyProcessed = new Hashtable(oldProcessed);
+			foreach (DictionaryEntry item in newProcessed) {
+				alreadyProcessed[item.Key] = 1;
+            }
+			outputs[0] = FindPathSub(directions[minIndex], target2, alreadyProcessed, newProcessedDeluxe[0], newPath, GetDistanceTravelledAway(distanceTravelledAway, currentPosition2, target2, directions[minIndex]), ref newTimes[0], ref state, ref count);
 			newPaths[0] = newPath;
 			if (outputs[0]) {
 				break;
@@ -213,11 +203,17 @@ public class tilePathFinder : MonoBehaviour {
 		}
 
 		// Possibly shorter overall, jumping can require getting further away initially
-		if (processed.Count <= maxTilesSearch) {
+		if (newProcessed.Count <= maxTilesSearch) {
 			if (jumpIndex != -1 && minIndex != jumpIndex) {
 				newPath = new List<Vector2Int>();
+				newProcessedDeluxe[1] = new Hashtable();
 				newTimes[1] = actionTimes[2];
-				outputs[1] = FindPathSub(directions[jumpIndex], target2, processed, newPath, GetDistanceTravelledAway(distanceTravelledAway, currentPosition2, target2, directions[jumpIndex]), ref newTimes[1], ref state);
+
+				Hashtable alreadyProcessed = new Hashtable(oldProcessed);
+				foreach (DictionaryEntry item in newProcessed) {
+					alreadyProcessed[item.Key] = 1;
+				}
+				outputs[1] = FindPathSub(directions[jumpIndex], target2, alreadyProcessed, newProcessedDeluxe[1], newPath, GetDistanceTravelledAway(distanceTravelledAway, currentPosition2, target2, directions[jumpIndex]), ref newTimes[1], ref state, ref count);
 				newPaths[1] = newPath;
 				pathCount++;
 			}
@@ -239,6 +235,9 @@ public class tilePathFinder : MonoBehaviour {
 
 		foreach (Vector2Int item in newPaths[minIndex]) {
 			path.Add(item);
+		}
+		foreach (DictionaryEntry item in newProcessedDeluxe[minIndex]) {
+			newProcessed[item.Key] = 1;
 		}
 		time += newTimes[minIndex];
 		return true;
@@ -268,5 +267,21 @@ public class tilePathFinder : MonoBehaviour {
 		if (tileName == null) return true;
 		if ((string)passableTilesIndex[tileName] == "1") return true;
 		return false;
+	}
+	private bool TestDirection(Vector2Int currentPosition2, Vector2Int testDirection2, ref Vector2Int direction2) {
+		Vector3Int testDirection3 = (Vector3Int)testDirection2;
+		Vector3Int currentPosition3 = (Vector3Int)currentPosition2;
+		bool canMove = false;
+		if (isPassable(GetTileName(currentPosition3 + Vector3Int.down))) {
+			if (isPassable(GetTileName(currentPosition3 + testDirection3 + Vector3Int.down))){
+				direction2 = currentPosition2 + testDirection2 + Vector2Int.down;
+				canMove = true;
+            }
+		}
+		else if (isPassable(GetTileName(currentPosition3 + testDirection3))) {
+			direction2 = currentPosition2 + testDirection2;
+			canMove = true;
+		}
+		return canMove;
 	}
 }
